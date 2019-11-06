5b210324e7fe15a9b3f524f57814c638877dd74b
pixies <pixies@riseup.net>
2019-11-06 17:24:38 -0300

    add em tudo

---
+++ ichat.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+site = Flask(__name__)
+
+
+@site.route("/index")
+def index():
+    return render_template("index.html")
+
+
+@site.route("/batepapo")
+def batepapo():
+    return render_template("paginas/bate_papo.html")
+
+
+@site.route("/cadastro")
+def cadastro():
+    return render_template("paginas/cadastro.html")
+
+
+@site.route("/login")
+def login():
+    return render_template("paginas/login.html")
+
+    
+
+
+site.run(debug=True)
+
+
+
+
+
+
+
