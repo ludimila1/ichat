3f06087f48cc76c2d17c28ba764b54ab46082755
Carmosoeles <carmosoeles16@gmail.com>
2019-11-21 16:27:55 -0300

    modifiquei o formulario.py

--- formulario.py
+++ formulario.py
@@ -1,8 +1,19 @@
  
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, InputRequired, EqualTo
 
 class CadastroForm(FlaskForm):
-    name = StringField('Login', validators=[DataRequired()])
-    
+    name = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', [InputRequired(), EqualTo('confirm', message='Senhas diferentes')])
+    confirme = PasswordField('Repita a senha')
+    botao = SubmitField('Enviar')
+
+class LoginForm(FlaskForm):
+    usuario = StringField('Usuário', validators=[DataRequired()])
+    senha = PasswordField('Senha', validators=[InputRequired()])
+    botao = SubmitField('Enviar')
+ 
+
+
