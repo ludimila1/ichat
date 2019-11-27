eee37e25c9670848c7b2b3e182ff1ce7e1de5d3e
pixies <pixies@riseup.net>
2019-11-27 17:10:39 -0300

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from formulario import CadastroForm
+from formulario import CadastroForm, MensagemForm
 
 from formulario import LoginForm
 from flask_sqlalchemy import SQLAlchemy
@@ -14,30 +14,64 @@ class Usuario(db.Model):
     usuario = db.Column(db.Text, nullable=False)
     senha = db.Column(db.Text, nullable=False)
     email = db.Column(db.Text, nullable=False)
+    #msg = db.relationship('Mensagens', backref='usario', lazy=True)
+
 
     def __repr__(self):
         return "usuario: %r" % self.usuario
 
+class Mensagem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    texto = db.Column(db.Text)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'),nullable=False)
+
+    def __repr__(self):
+        return "%r" % self.texto
+
+@app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html")
 
-@app.route("/batepapo")
+@app.route("/batepapo", methods=['POST','GET'])
 def batepapo():
-    return render_template("paginas/bate_papo.html")
+    form = MensagemForm()
+    if form.validate_on_submit():
+        msg = Mensagem()
+        msg.texto = form.texto.data
+        msg.usuario_id = 2
+        db.session.add(msg)
+        db.session.commit()
+    
+    mensagens = Mensagem.query.all()
+    return render_template("paginas/bate_papo.html", form=form, mensagens=mensagens)
 
 @app.route("/cadastro", methods=["GET", "POST"])
 def cadastro():
-    formulario=CadastroForm()
-    if formulario.validate_on_submit():
-        u=Usuario()
-        u.usuario=formulario.name.data
-        u.email = formulario.email.data
-        u.senha = formulario.senha.data
-        db.session.add(u)
-        db.sessoin.commit()
 
-    return render_template("paginas/cadastro.html", form=formulario)
+    form = CadastroForm()
+    print(form.validate_on_submit())
+    if form.validate_on_submit():
+        user1 = Usuario()
+        user1.usuario = form.usuario.data
+        user1.email = form.email.data
+        user1.senha = form.senha.data
+        db.session.add(user1)
+        db.session.commit()
+
+    '''
+    formulario=CadastroForm()
+    print(formulario.data)
+    print(formulario.validate_on_submit())
+    #if formulario.validate_on_submit():
+    u=Usuario()
+    u.usuario=formulario.usuario.data
+    u.email = formulario.email.data
+    u.senha = formulario.senha.data
+    db.session.add(u)
+    db.session.commit()
+    '''
+    return render_template("paginas/cadastro.html", form=form)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -48,4 +82,4 @@ def login():
     return render_template("paginas/login.html",  form=formulario)
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8888)
